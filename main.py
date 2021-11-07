632ccff3df93fe28ab02d8cc801ed232a0c0b8c4
gaogaogao666 <66062358+gaogaogao666@users.noreply.github.com>
2021-11-07 15:49:38 +0100

    part1,2

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import tempfile
 import shutil
 
 from numpy.core._methods import _mean
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession
 from pyspark.mllib.stat import Statistics
 from pyspark.mllib.util import MLUtils
 from pyspark.sql.functions import map_filter, array_max, col
+from pyspark.sql.types import DoubleType
 
 spark = SparkSession.builder.appName("main").getOrCreate()
 path = "/Users/gaogao/PycharmProjects/pythonProject/housing.csv"
@@ -53,3 +56,26 @@ housing.createOrReplaceTempView("df")
 sqldf = spark.sql("SELECT ocean_proximity,avg(median_house_value) as avg_value FROM df GROUP by ocean_proximity")
 sqldf.collect()
 sqldf.show()
+
+#2-5
+housing_0=housing\
+    .withColumn("median_house_value",housing["median_house_value"].cast('float'))\
+    .withColumn("total_rooms",housing["total_rooms"].cast('float'))\
+    .withColumn("housing_median_age",housing["housing_median_age"].cast('float'))\
+    .withColumn("population",housing["population"].cast('float'))
+housing_0.printSchema()
+va = VectorAssembler(outputCol="features")
+va.setInputCols(["median_house_value","total_rooms","housing_median_age", "population"])
+features_housing_0=va.transform(housing_0)
+features_housing_0.show()
+features_housing_0.select('features').show()
+
+corr1=Correlation.corr(features_housing_0,'features','pearson').head()
+print("pearson correlation matrix : "+str(corr1[0]))
+
+#2-6
+housingCol1=housing.withColumn('rooms_per_household',housing.total_rooms/housing.households)
+housingCol2=housingCol1.withColumn('bedrooms_per_room',housingCol1.total_bedrooms/housingCol1.total_rooms)
+housingExtra=housingCol2.withColumn('population_per_household',housing.population/housing.households)
+housingExtra.show(5)
+
